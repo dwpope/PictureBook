6d3070c70d9ff09a998a7190093e7ab59af9d3a8
dwpope <dwpopesyd@gmail.com>
2021-01-09 14:53:33 +0000

    Test - Added a rectangle

--- PictureBook/Pages/StoryPage.swift
+++ PictureBook/Pages/StoryPage.swift
@@ -19,6 +19,7 @@ struct StoryPage: View {
             VStack {
                 Text("There are many special things in this world, my love, but the most special thing is you.")
                     .font(.body)
+                Rectangle()
             }
             .padding(32)
         }
